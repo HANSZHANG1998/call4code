1297139c831b2bdb198bdb938036af35c88f1d4b
HANSZHANG1998 <hanswilson1234@gmail.com>
2020-06-25 07:50:52 +0800

    2.0

--- index.php
+++ index.php
@@ -6,17 +6,17 @@
 
 	<!-- 所有地图数据详情见：https://img.hcharts.cn/mapdata/index.html -->
 
-<link rel="icon" href="https://jscdn.com.cn/highcharts/images/favicon.ico">
-<link href="https://img.hcharts.cn/libs/jquery-ui/themes/base/jquery-ui.css" rel="stylesheet">
-<link href="https://img.hcharts.cn/libs/font-awesome/css/font-awesome.css" rel="stylesheet">
-<link href="index.css" rel="stylesheet">
-<script src="https://code.highcharts.com.cn/jquery/jquery-1.8.3.min.js"></script>
-<script src="https://code.highcharts.com.cn/highmaps/highmaps.js"></script>
-<script src="https://code.highcharts.com.cn/highcharts/modules/exporting.js"></script>
-<script src="https://img.hcharts.cn/mapdata/index.js"></script>
-<script src="https://img.hcharts.cn/libs/jquery-ui/jquery-ui.js"></script>
-<script src="https://img.hcharts.cn/static/common/jquery.combobox.js"></script>
-<script src="main.js"></script>
+<link rel="icon" href="static/img/favicon.ico">
+<link href="static/css/jquery-ui.css" rel="stylesheet">
+<link href="static/css/font-awesome.css" rel="stylesheet">
+<link href="static/css/index.css" rel="stylesheet">
+<script src="static/js/jquery.min.js"></script>
+<script src="static/js/highmaps.js"></script>
+<script src="static/js/exporting.js"></script>
+<script src="static/js/index.js"></script>
+<script src="static/js/jquery-ui.js"></script>
+<script src="static/js/jquery.combobox.js"></script>
+<script src="static/js/main.js"></script>
 
 </head>
 <body>
--- static/js/main.js
+++ static/js/main.js
@@ -1,51 +1,22 @@
 	$(function () {
-	/**
-     * This is a complicated demo of Highmaps, not intended to get you up to speed
-     * quickly, but to show off some basic maps and features in one single place.
-     * For the basic demo, check out http://www.highcharts.com/maps/demo/geojson instead.
-     */
+
 	// Base path to maps
-	var baseMapPath = "https://img.hcharts.cn/mapdata/",
-		showDataLabels = false, // Switch for data labels enabled/disabled
+	var showDataLabels = false, // Switch for data labels enabled/disabled
 		mapCount = 0,
 		searchText,
 		mapOptions = '';
-	// Populate dropdown menus and turn into jQuery UI widgets
-	$.each(Highcharts.mapDataIndex, function (mapGroup, maps) {
-		if (mapGroup !== "version") {
-			mapOptions += '<option class="option-header">' + mapGroup + '</option>';
-			if(mapGroup === 'Countries') {
-				delete maps.Taiwan;
-			}
-			$.each(maps, function (desc, path) {
-				mapOptions += '<option value="' + path + '">' + desc + '</option>';
-				mapCount += 1;
-			});
-		}
-	});
-	searchText = 'Search ' + mapCount + ' maps';
-	mapOptions = '<option value="custom/world.js">' + searchText + '</option>' + mapOptions;
+	  searchText = 'Search ' + mapCount + ' maps';
+	  mapOptions = '<option value="custom/world.js">' + searchText + '</option>' + mapOptions;
 	$("#mapDropdown").append(mapOptions).combobox();
 	// Change map when item selected in dropdown
 	$("#mapDropdown").change(function () {
 		var $selectedItem = $("option:selected", this),
 			mapDesc = $selectedItem.text(),
 			mapKey = this.value.slice(0, -3),
-			svgPath = baseMapPath + mapKey + '.svg',
 			geojsonPath = 'geojson.json',
-			javascriptPath = baseMapPath + this.value,
+			javascriptPath = 'static/js/world.js',
 			isHeader = $selectedItem.hasClass('option-header');
-		// Dim or highlight search box
-		if (mapDesc === searchText || isHeader) {
-			$('.custom-combobox-input').removeClass('valid');
-			location.hash = '';
-		} else {
-			$('.custom-combobox-input').addClass('valid');
-			location.hash = mapKey;
-		}
-		if (isHeader) {
-			return false;
-		}
+
 		// Show loading
 		if ($("#container").highcharts()) {
 			$("#container").highcharts().showLoading('<i class="fa fa-spinner fa-spin fa-2x"></i>');
@@ -56,15 +27,7 @@
 				data = [],
 				parent,
 				match;
-			// Update info box download links
-			$("#download").html(
-				'<a class="button" target="_blank" href="https://code.hcharts.cn/mapdata/CN4fQa?map=' + mapKey + '">' +
-				'查看单个实例</a>' +
-				'<div class="or-view-as"> 查看详细的数据文件： ' +
-				'<a target="_blank" href="' + svgPath + '">SVG</a>, ' +
-				'<a target="_blank" href="' + geojsonPath + '">GeoJSON</a>, ' +
-				'<a target="_blank" href="' + javascriptPath + '">JavaScript</a>.</div>'
-			);
+
 			// Generate non-random data for the map
 			$.each(mapGeoJSON.features, function (index, feature) {
 				data.push({
@@ -72,11 +35,7 @@
 					value: showdata(feature.properties['iso-a3'])
 				});
 			});
-			// Show arrows the first time a real map is shown
-			if (mapDesc !== searchText) {
-				$('.selector .prev-next').show();
-				$('#sideBox').show();
-			}
+
 			// Is there a layer above this?
 			match = mapKey.match(/^(countries\/[a-z]{2}\/[a-z]{2})-[a-z0-9]+-all$/);
 			if (/^countries\/[a-z]{2}\/[a-z]{2}-all$/.test(mapKey)) { // country
@@ -190,9 +149,7 @@ function showdata(data){
 					 restring = restring.substring(1,restring.length);
 	         res = parseInt(restring,10);
 				 }
-
-  }
-});
-
+		  }
+		});
  return res;
 }
